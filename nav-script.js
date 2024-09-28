d78bde0b4776b3332a0568d6bf7a93a4052a507f
Manoj <manojkumarponnana@gmail.com>
2024-09-28 17:52:50 +0530

    inital commit

---
+++ nav-script.js
@@ -0,0 +1,7 @@
+function appear(){
+    document.getElementsByClassName('nav-bar-1')[0].style.display='block';
+    document.getElementsByClassName('nav-bar-1')[0].style.right='0';
+}
+function disappear(){
+    document.getElementsByClassName('nav-bar-1')[0].style.display='none';
+}
